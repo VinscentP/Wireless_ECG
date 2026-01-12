ba4497e99bac34ad026b9f89dee1198d93f28649
Vinscent <vincentpham3058@gmail.com>
2026-01-12 11:25:42 -0800

    Fixed moving average using circular buffer

--- signal_process/moving_average.cpp
+++ signal_process/moving_average.cpp
@@ -1,3 +1,31 @@
+#include <vector>
+
+using namespace std;
+
 class MovingAverage {
     private:
+        vector<float> buffer;
+        float sum;
+        int window_size;
+        int count;
+        int index;
+
+        
+    public:
+        MovingAverage(float window_size) :
+         sum(0), index(0), count(0), 
+         buffer(window_size, 0),
+         window_size(window_size){}
+
+        float moving_average(float input){
+            sum -= buffer[index];
+            buffer[index] = input;
+            sum += input;
+            index = (index + 1) % window_size;
+            
+            if(count < window_size) count++;
+            
+            return sum / count;
+        }
+
 };
---
+++ signal_process/peak_detection.cpp
@@ -0,0 +1,3 @@
+class PeakDetection{
+    private:
+};
