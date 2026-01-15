e27e898bdd91d8ed1979416216be6c8c54233e70
Vinscent <vincentpham3058@gmail.com>
2026-01-15 00:35:39 -0800

    committing peak_detection.cpp

--- signal_process/peak_detection.cpp
+++ signal_process/peak_detection.cpp
@@ -1,3 +1,24 @@
-class PeakDetection{
+#include <vector>
+
+using namespace std;
+class PeakDetector{
     private:
+        vector<float> buffer;
+        vector<int> r_peak_indices;
+        float prev_sample, next_sample;
+
+        int sample_counter;
+
+    public:
+    PeakDetector(): prev_sample(0), next_sample(0), sample_counter(0){}
+    int peakDetection(float input){
+        
+    }
+    void addPeak(int index){
+        r_peak_indices.push_back(index);
+    }
+
+    const vector<int>& getPeaks(){
+        return r_peak_indices;
+    }
 };
